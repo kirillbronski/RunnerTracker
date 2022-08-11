c34dd962daf87e059db4b552b14ab16bb7159258
Kirill <bronskidev@gmail.com>
2022-08-11 20:40:45 +0300

    setup manifest. create application class

---
+++ app/src/main/java/com/bronski/android/runnertracker/core/App.kt
@@ -0,0 +1,30 @@
+package com.bronski.android.runnertracker.core
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        setupTimber()
+    }
+
+    private fun setupTimber() {
+        Timber.plant(object : Timber.DebugTree() {
+            override fun createStackElementTag(element: StackTraceElement): String? {
+                return String.format("%s: %s: %s",
+                    element.fileName,
+                    element.methodName,
+                    element.lineNumber,
+                    super.createStackElementTag(element))
+            }
+
+            override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                super.log(priority, "Timber: $tag", message, t)
+            }
+        })
+    }
+}
