6a47bdba0a9e88948d2c77bf96baf5a91f7dd912
Kirill <kirillbronski@gmail.com>
2022-08-13 21:26:28 +0300

    add design to all fragments

--- app/src/main/java/com/bronski/android/runnertracker/setup/ui/SetupFragment.kt
+++ app/src/main/java/com/bronski/android/runnertracker/setup/ui/SetupFragment.kt
@@ -1,6 +1,10 @@
 package com.bronski.android.runnertracker.setup.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.bronski.android.runnertracker.R
 import com.bronski.android.runnertracker.core.ui.BaseFragment
 import com.bronski.android.runnertracker.databinding.FragmentSetupBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -9,6 +13,13 @@ import dagger.hilt.android.AndroidEntryPoint
 class SetupFragment : BaseFragment<FragmentSetupBinding>() {
 
     private val viewModel by viewModels<SetupViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.continueTextView.setOnClickListener {
+            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+        }
+    }
 
     override fun getViewBinding() = FragmentSetupBinding.inflate(layoutInflater)
 
