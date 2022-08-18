c08b3eba12bf7657e8b620f2f084893e04f7bf1a
Kirill <bronskidev@gmail.com>
2022-08-18 21:28:39 +0300

    Updating the Notification 1

---
+++ app/src/main/java/com/bronski/android/runnertracker/di/modules/ServiceModule.kt
@@ -0,0 +1,10 @@
+package com.bronski.android.runnertracker.di.modules
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+}
